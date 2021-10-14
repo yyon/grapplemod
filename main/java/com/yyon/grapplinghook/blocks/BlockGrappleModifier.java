0d7ec035e723c633cf2ee2be4f7bba7174cf5aed
yyon <linkkirby7@gmail.com>
2021-10-13 21:51:28 -0400

    Added easter egg

--- main/java/com/yyon/grapplinghook/blocks/BlockGrappleModifier.java
+++ main/java/com/yyon/grapplinghook/blocks/BlockGrappleModifier.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import com.yyon.grapplinghook.GrappleConfig;
 import com.yyon.grapplinghook.GrappleCustomization;
 import com.yyon.grapplinghook.grapplemod;
+import com.yyon.grapplinghook.vec;
 import com.yyon.grapplinghook.items.grappleBow;
 import com.yyon.grapplinghook.items.upgrades.BaseUpgradeItem;
 
@@ -17,12 +18,15 @@ import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.projectile.FireworkRocketEntity;
+import net.minecraft.item.FireworkRocketItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.loot.LootContext;
 import net.minecraft.loot.LootParameters;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.ListNBT;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
@@ -122,6 +126,8 @@ public class BlockGrappleModifier extends Block {
 					playerIn.sendMessage(new StringTextComponent("Making long fall boots this way was disabled in the config. It probably has been replaced by a crafting recipe."), playerIn.getUUID());
 				}
 			}
+		} else if (helditem == Items.DIAMOND) {
+			this.easterEgg(state, worldIn, pos, playerIn, hand, raytraceresult);
 		} else {
 			if (worldIn.isClientSide) {
 				TileEntity ent = worldIn.getBlockEntity(pos);
@@ -131,5 +137,40 @@ public class BlockGrappleModifier extends Block {
 			}
 		}
 		return ActionResultType.SUCCESS;
+	}
+
+	public void easterEgg(BlockState state, World worldIn, BlockPos pos, PlayerEntity playerIn, Hand hand,
+			BlockRayTraceResult raytraceresult) {
+		int spacing = 3;
+		vec[] positions = new vec[] {new vec(-spacing*2, 0, 0), new vec(-spacing, 0, 0), new vec(0, 0, 0), new vec(spacing, 0, 0), new vec(2*spacing, 0, 0)};
+		int[] colors = new int[] {0x5bcffa, 0xf5abb9, 0xffffff, 0xf5abb9, 0x5bcffa};
+		
+		for (int i = 0; i < positions.length; i++) {
+			vec newpos = new vec(pos.getX() + 0.5, pos.getY() + 1, pos.getZ() + 0.5);
+			vec toPlayer = vec.positionvec(playerIn).sub(newpos);
+			double angle = toPlayer.length() == 0 ? 0 : toPlayer.getYaw();
+			newpos = newpos.add(positions[i].rotate_yaw(Math.toRadians(angle)));
+			
+			CompoundNBT explosion = new CompoundNBT();
+	        explosion.putByte("Type", (byte) FireworkRocketItem.Shape.SMALL_BALL.getId());
+	        explosion.putBoolean("Trail", true);
+	        explosion.putBoolean("Flicker", false);
+	        explosion.putIntArray("Colors", new int[] {colors[i]});
+	        explosion.putIntArray("FadeColors", new int[] {});
+	        ListNBT list = new ListNBT();
+	        list.add(explosion);
+	        CompoundNBT fireworks = new CompoundNBT();
+	        fireworks.put("Explosions", list);
+	        CompoundNBT nbt = new CompoundNBT();
+	        nbt.put("Fireworks", fireworks);
+	        ItemStack stack = new ItemStack(Items.FIREWORK_ROCKET);
+	        stack.setTag(nbt);
+			FireworkRocketEntity firework = new FireworkRocketEntity(worldIn, playerIn, newpos.x, newpos.y, newpos.z, stack);
+			CompoundNBT fireworksave = new CompoundNBT();
+			firework.addAdditionalSaveData(fireworksave);
+			fireworksave.putInt("LifeTime", 15);
+			firework.readAdditionalSaveData(fireworksave);
+			worldIn.addFreshEntity(firework);
+		}
 	}
 }
