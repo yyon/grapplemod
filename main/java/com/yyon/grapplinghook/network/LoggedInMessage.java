6409f0009446f52b3cc83c5bd950f6592ae9c8d8
yyon <linkkirby7@gmail.com>
2021-10-16 18:03:33 -0400

    Fixed bug: server config options not syncing to client

--- main/java/com/yyon/grapplinghook/network/LoggedInMessage.java
+++ main/java/com/yyon/grapplinghook/network/LoggedInMessage.java
@@ -6,6 +6,7 @@ import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.Comparator;
 
+import com.yyon.grapplinghook.grapplemod;
 import com.yyon.grapplinghook.config.GrappleConfig;
 
 import net.minecraft.network.PacketBuffer;
@@ -31,7 +32,7 @@ import net.minecraftforge.fml.network.NetworkEvent;
  */
 
 public class LoggedInMessage extends BaseMessageClient {
-    GrappleConfig.Config conf = null;
+    GrappleConfig.Config conf;
 
     public LoggedInMessage(PacketBuffer buf) {
     	super(buf);
@@ -40,10 +41,9 @@ public class LoggedInMessage extends BaseMessageClient {
     public LoggedInMessage(GrappleConfig.Config serverconf) {
     	this.conf = serverconf;
     }
-
-    public void decode(PacketBuffer buf) {
-    	Class<GrappleConfig.Config> confclass = GrappleConfig.Config.class;
-    	Field[] fields = confclass.getDeclaredFields();
+    
+    public <T> void decodeClass(PacketBuffer buf, Class<T> theClass, T theObject) {
+    	Field[] fields = theClass.getDeclaredFields();
     	Arrays.sort(fields, new Comparator<Field>() {
     	    @Override
     	    public int compare(Field o1, Field o2) {
@@ -51,21 +51,64 @@ public class LoggedInMessage extends BaseMessageClient {
     	    }
     	});
     	
+    	for (Field field : fields) {
+    		Type fieldtype = field.getGenericType();
+    		try {
+        		if (fieldtype.getTypeName().equals("int")) {
+        			field.setInt(theObject, buf.readInt());
+        		} else if (fieldtype.getTypeName().equals("double")) {
+        			field.setDouble(theObject, buf.readDouble());
+        		} else if (fieldtype.getTypeName().equals("boolean")) {
+        			field.setBoolean(theObject, buf.readBoolean());
+        		} else if (fieldtype.getTypeName().equals("java.lang.String")) {
+        			int len = buf.readInt();
+        			CharSequence charseq = buf.readCharSequence(len, Charset.defaultCharset());
+        			field.set(theObject, charseq.toString());
+        		} else if (field.getType() != null && Object.class.isAssignableFrom(field.getType())) {
+        			Class newClass = field.getType();
+        			decodeClass(buf, newClass, newClass.cast(field.get(theObject)));
+        		} else {
+        			System.out.println("Unknown Type");
+        			System.out.println(fieldtype.getTypeName());
+        		}
+    		} catch (IllegalAccessException e) {
+    			System.out.println(e);
+    		}
+    	}
+    }
+
+    public void decode(PacketBuffer buf) {
+    	Class<GrappleConfig.Config> confclass = GrappleConfig.Config.class;
     	this.conf = new GrappleConfig.Config();
+    	
+    	decodeClass(buf, confclass, this.conf);
+    }
+
+    public <T> void encodeClass(PacketBuffer buf, Class<T> theClass, T theObject) {
+    	Field[] fields = theClass.getDeclaredFields();
+    	Arrays.sort(fields, new Comparator<Field>() {
+    	    @Override
+    	    public int compare(Field o1, Field o2) {
+    	        return o1.getName().compareTo(o2.getName());
+    	    }
+    	});
     	
     	for (Field field : fields) {
     		Type fieldtype = field.getGenericType();
     		try {
         		if (fieldtype.getTypeName().equals("int")) {
-        			field.setInt(this.conf, buf.readInt());
+        			buf.writeInt(field.getInt(theObject));
         		} else if (fieldtype.getTypeName().equals("double")) {
-        			field.setDouble(this.conf, buf.readDouble());
+        			buf.writeDouble(field.getDouble(theObject));
         		} else if (fieldtype.getTypeName().equals("boolean")) {
-        			field.setBoolean(this.conf, buf.readBoolean());
+        			buf.writeBoolean(field.getBoolean(theObject));
         		} else if (fieldtype.getTypeName().equals("java.lang.String")) {
-        			int len = buf.readInt();
-        			CharSequence charseq = buf.readCharSequence(len, Charset.defaultCharset());
-        			field.set(this.conf, charseq.toString());
+        			String str = (String) field.get(theObject);
+        			buf.writeInt(str.length());
+        			buf.writeCharSequence(str.subSequence(0, str.length()), Charset.defaultCharset());
+        		} else if (field.getType() != null && Object.class.isAssignableFrom(field.getType())) {
+        			Class newClass = field.getType();
+        			encodeClass(buf, newClass, newClass.cast(field.get(theObject)));
         		} else {
         			System.out.println("Unknown Type");
         			System.out.println(fieldtype.getTypeName());
@@ -78,35 +121,7 @@ public class LoggedInMessage extends BaseMessageClient {
 
     public void encode(PacketBuffer buf) {
     	Class<GrappleConfig.Config> confclass = GrappleConfig.Config.class;
-    	Field[] fields = confclass.getDeclaredFields();
-    	Arrays.sort(fields, new Comparator<Field>() {
-    	    @Override
-    	    public int compare(Field o1, Field o2) {
-    	        return o1.getName().compareTo(o2.getName());
-    	    }
-    	});
-    	
-    	for (Field field : fields) {
-    		Type fieldtype = field.getGenericType();
-    		try {
-        		if (fieldtype.getTypeName().equals("int")) {
-        			buf.writeInt(field.getInt(this.conf));
-        		} else if (fieldtype.getTypeName().equals("double")) {
-        			buf.writeDouble(field.getDouble(this.conf));
-        		} else if (fieldtype.getTypeName().equals("boolean")) {
-        			buf.writeBoolean(field.getBoolean(this.conf));
-        		} else if (fieldtype.getTypeName().equals("java.lang.String")) {
-        			String str = (String) field.get(this.conf);
-        			buf.writeInt(str.length());
-        			buf.writeCharSequence(str.subSequence(0, str.length()), Charset.defaultCharset());
-        		} else {
-        			System.out.println("Unknown Type");
-        			System.out.println(fieldtype.getTypeName());
-        		}
-    		} catch (IllegalAccessException e) {
-    			System.out.println(e);
-    		}
-    	}
+    	encodeClass(buf, confclass, this.conf);
     }
 
     @OnlyIn(Dist.CLIENT)
