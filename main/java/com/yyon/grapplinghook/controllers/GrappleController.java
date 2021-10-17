265bf528ca7d83458fa1eb27861c461699604f0f
yyon <linkkirby7@gmail.com>
2021-10-17 16:09:49 -0400

    don't yank the player if the hook is slightly over max length when it attaches due to some miscalculation

--- main/java/com/yyon/grapplinghook/controllers/GrappleController.java
+++ main/java/com/yyon/grapplinghook/controllers/GrappleController.java
@@ -200,7 +200,7 @@ public class GrappleController {
 						// vectors along rope
 						Vec anchor = hookEntity.segmentHandler.getClosest(hookPos);
 						double distToAnchor = hookEntity.segmentHandler.getDistToAnchor();
-						double remaininglength = motor ? this.custom.maxlen - distToAnchor : hookEntity.r - distToAnchor;
+						double remaininglength = motor ? Math.max(this.custom.maxlen, hookEntity.r) - distToAnchor : hookEntity.r - distToAnchor;
 						
 						Vec oldspherevec = playerpos.sub(anchor);
 						Vec spherevec = oldspherevec.changeLen(remaininglength);
